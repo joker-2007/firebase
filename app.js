6b9c85a7d381e916c1bb9c8d590b3b9465627656
joker-2007 <76997218+joker-2007@users.noreply.github.com>
2021-04-18 20:13:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+
+
+    
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBacwMog1hwTDP_aS8qreZHX8cf4MZMITQ",
+    authDomain: "skill-dev-cloud.firebaseapp.com",
+    databaseURL: "https://skill-dev-cloud-default-rtdb.firebaseio.com",
+    projectId: "skill-dev-cloud",
+    storageBucket: "skill-dev-cloud.appspot.com",
+    messagingSenderId: "42828431492",
+    appId: "1:42828431492:web:54815d4350dc4e9ccb82d1",
+    measurementId: "G-03G205DHJW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
 
 // DEfault configuration - Change these if you have a different STUN or TURN server.
