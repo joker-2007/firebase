ab4621c5a5c20c273d13dd12b3f63336ca08025b
joker-2007 <76997218+joker-2007@users.noreply.github.com>
2021-04-19 10:58:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ const hangupButton = document.getElementById('hangupButton');
 // 1. Setup media sources
 
 webcamButton.onclick = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localStream = navigator.mediaDevices.getUserMedia({audio:true, video:true})
   remoteStream = new MediaStream();
 
   // Push tracks from local stream to peer connection
