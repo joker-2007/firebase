e7f21ab653e9b635e415bdb79f9d7c4349002c19
joker-2007 <76997218+joker-2007@users.noreply.github.com>
2021-04-19 10:45:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ import 'firebase/firestore';
 
 const firebaseConfig = {
   // your config
+  apiKey: "AIzaSyBacwMog1hwTDP_aS8qreZHX8cf4MZMITQ",
+    authDomain: "skill-dev-cloud.firebaseapp.com",
+    databaseURL: "https://skill-dev-cloud-default-rtdb.firebaseio.com",
+    projectId: "skill-dev-cloud",
+    storageBucket: "skill-dev-cloud.appspot.com",
+    messagingSenderId: "42828431492",
+    appId: "1:42828431492:web:54815d4350dc4e9ccb82d1",
+    measurementId: "G-03G205DHJW"
 };
 
 if (!firebase.apps.length) {
